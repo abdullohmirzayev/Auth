899afddedeb35049cb69ec2577b5110e73b8856a
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-05-16 18:10:38 +0500

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { App } from './components';
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
